06bf592571f4e83c089936baec801126a423f31a
DingleberryV2 <Jwintring2@gmail.com>
2023-03-13 21:16:47 -0700

    Lil balls

--- src/main/java/net/jacob/tutorialmod/TutorialMod.java
+++ src/main/java/net/jacob/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.jacob.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jacob.tutorialmod.item.Moditems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,6 +19,8 @@ public class TutorialMod {
 
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        Moditems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/jacob/tutorialmod/item/Moditems.java
@@ -0,0 +1,22 @@
+package net.jacob.tutorialmod.item;
+
+import net.jacob.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> CHLOROPHYTE = ITEMS.register("chlorophyte",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
