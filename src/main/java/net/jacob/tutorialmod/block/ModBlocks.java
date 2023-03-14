e0cfc12c667ca4d3be3e2abaa819d665d9ae843f
DingleberryV2 <Jwintring2@gmail.com>
2023-03-14 13:47:54 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/resources/assets/tutorialmod/lang/en.us.json

--- src/main/java/net/jacob/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/jacob/tutorialmod/block/ModBlocks.java
@@ -43,7 +43,7 @@ public class ModBlocks {
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
                                                                             CreativeModeTab tab) {
-        return Moditems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)))
+        return Moditems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 
     public static void register(IEventBus eventBus) {
